bad34922f66829be8f01dccd7db4d2bae68e70c6
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-07-27 12:36:36 +0800

    formatter

--- Core30/Program.cs
+++ Core30/Program.cs
@@ -1,4 +1,9 @@
-﻿using Natasha;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Options;
+using Natasha;
 using System;
 
 namespace Core30
@@ -16,49 +21,74 @@ namespace Core30
              */
 
 
-            string text = @"namespace HelloWorld
-{
-    public class Test
-    {
-        public Test(){
-            Name=""111"";
-        }
+//            string text = @"namespace HelloWorld
+//{
+//    public class Test
+//    {
+//        public Test(){
+//            Name=""111"";
+//        }
 
-        public string Name;
-        public int Age{get;set;}
-    }
+//        public string Name;
+//        public int Age{get;set;}
+//    }
+//}";
+//            //根据脚本创建动态类
+//            Type type = ClassBuilder.GetType(text);
+//            //创建动态类实例代理
+//            DynamicOperator instance = type;
+
+//            if (instance["Name"].StringValue == "111")
+//            {
+//                //调用动态委托赋值
+//                instance["Name"].StringValue = "222";
+//            }
+//            //调用动态类
+//            Console.WriteLine(instance["Name"].StringValue);
+
+//            TestB b = new TestB();
+//            b.Name = "abc";
+//            var result = CloneOperator.Clone(b);
+
+
+//            //创建动态类实例代理
+//            DynamicOperator<TestB> instance2 = new DynamicOperator<TestB>();
+
+//            if (instance2["Name"].StringValue == "111")
+//            {
+//                //调用动态委托赋值
+//                instance2["Name"].StringValue = "222";
+//            }
+//            //调用动态类
+//            Console.WriteLine(instance2["Name"].StringValue);
+
+
+
+            var source =
+           @"class A
+{private struct S { 
+}
 }";
-            //根据脚本创建动态类
-            Type type = ClassBuilder.GetType(text);
-            //创建动态类实例代理
-            DynamicOperator instance = type;
 
-            if (instance["Name"].StringValue == "111")
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(source);
+            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+
+            AdhocWorkspace adhoc = new AdhocWorkspace();
+            adhoc.AddSolution(SolutionInfo.Create(SolutionId.CreateNewId("test"),VersionStamp.Default));
+            
+            foreach (var item in adhoc.Services.SupportedLanguages)
             {
-                //调用动态委托赋值
-                instance["Name"].StringValue = "222";
+                Console.WriteLine(item);
             }
-            //调用动态类
-            Console.WriteLine(instance["Name"].StringValue);
 
-            TestB b = new TestB();
-            b.Name = "abc";
-            var result = CloneOperator.Clone(b);
-
-
-            //创建动态类实例代理
-            DynamicOperator<TestB> instance2 = new DynamicOperator<TestB>();
-
-            if (instance2["Name"].StringValue == "111")
-            {
-                //调用动态委托赋值
-                instance2["Name"].StringValue = "222";
-            }
-            //调用动态类
-            Console.WriteLine(instance2["Name"].StringValue);
+            var temp = Formatter.Format(root, adhoc);
+            Console.WriteLine(temp.ToString());
+            Console.ReadKey();
 
 
             Console.ReadKey();
+
         }
     }
     public class TestB
---
+++ Core30/TestModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core30
+{
+
+    public abstract class AbsTest {
+
+        public abstract T Get<T>();
+    }
+    //public class TestModel:AbsTest
+    //{
+    //    public int Age;
+    //    public string Name;
+
+    //    public override T Get<T>()
+    //    {
+    //        if (Age is T)
+    //        {
+    //            return Age;
+    //        }
+    //        else if (Name is T)
+    //        {
+    //            return Name;
+    //        }
+    //    }
+    //}
+}
